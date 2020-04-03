27bc4b61ed15ab48c1b3673ae4c48e86d28e1827
yang052413 <liyang0525.ly@gmail.com>
2020-04-02 17:57:04 -0700

    complete v1 done

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
 # animeKnow
 
-## What is animeKnow?
+## 介绍
 
-animeKnow is a Web App that built with React.js framework and recommends featured animation from Bangumi API. Bangumi API has tons of animation database which is very useful. Sometime you dont know what to watch? animeKnow helps you find the animation for you. 
+animeKnow is a Web App that built with React.js framework utilized Bangumi API. Bangumi API has tons of subjects database which includes animation, music, books and others. Sometime you dont know what to watch? animeKnow helps you find the animation for you.
 
-## Features 
+## 功能
+- 实时更新同步每日放送的新番列表
+- 搜寻上万条目包括动画，音乐，三次元，游戏，书籍种类
+- 随机推荐Banguimi前1000名动画作品
+- 收藏您喜欢的新番到个人账号
 
-## Tools
+### 预览
+<i style="font-size: 13px">每日放送新番列表</i>
+![avatar](public/ref/1.png)
+
+<i style="font-size: 13px">搜索条目</i>
+![avatar](public/ref/2.png)
+
+<i style="font-size: 13px">搜索条目返回结果</i>
+![avatar](public/ref/3.png)
+
+<i style="font-size: 13px">系统推荐新番</i>
+![avatar](public/ref/4.png)
+
+<i style="font-size: 13px">系统推荐新番</i>
+![avatar](public/ref/5.png)
+
+
+## 技术
 - HTML5, CSS3
 - javaScript
 - jQuery
--- src/App.js
+++ src/App.js
@@ -14,27 +14,74 @@ class App extends Component {
 			recommShow: false,
 			aboutShow: false
         }
-        this.showHandler = this.showHandler.bind(this)
+		// this.showHandler = this.showHandler.bind(this)
+		this.handleAir = this.handleAir.bind(this)
+		this.handleSearch = this.handleSearch.bind(this)
+		this.handleRecomm = this.handleRecomm.bind(this)
+		this.handleAbout = this.handleAbout.bind(this)
 	}
 	
-	showHandler(event) {
-		const {name} = event.target
-        this.setState(prevState => {
-			return {
-				[name]: !prevState[name]
-			}
+	handleAir() {
+		this.setState({
+			airShow: !this.state.airShow,
+			searchShow: false,
+			recommShow: false,
+			aboutShow: false
 		})
-    }
+	}
+
+	handleSearch() {
+		this.setState({
+			airShow: false,
+			searchShow: !this.state.searchShow,
+			recommShow: false,
+			aboutShow: false
+		})
+	}
+
+	handleRecomm() {
+		this.setState({
+			airShow: false,
+			searchShow: false,
+			recommShow: !this.state.recommShow,
+			aboutShow: false
+		})
+	}
+
+	handleAbout() {
+		this.setState({
+			airShow: false,
+			searchShow: false,
+			recommShow: false,
+			aboutShow: !this.state.aboutShow
+		})
+	}
+
+	// showHandler(event) {
+	// 	const {name} = event.target
+
+    //     this.setState(prevState => {
+	// 		return {
+	// 			[name]: !prevState[name]
+	// 		}
+	// 	})
+    // }
 
 	render() {
+		let navStyle = {
+			borderBottom: "1px solid rgb(135, 70, 70)",
+    		color: "rgb(135, 70, 70)",
+    		paddingBottom: "5px"
+		}
+
   		return (
     		<div className="App">
 				<div className="navbar">
-                	<a href="#main-page" name="airShow" onClick={this.showHandler}>主页</a>
-                	<a href="#search-page" name="searchShow" onClick={this.showHandler}>搜寻</a>
+                	<a style={this.state.airShow ? navStyle :  {} } href="#main-page" name="airShow" onClick={this.handleAir}>主页</a>
+                	<a style={this.state.searchShow ? navStyle :  {} } href="#search-page" name="searchShow" onClick={this.handleSearch}>搜寻</a>
                 	<img src="./image/user.jpg" alt="user profile picture" />
-                	<a href="#recommendation-page" name="recommShow" onClick={this.showHandler}>推荐</a>
-                	<a href="#about-page" name="aboutShow" onClick={this.showHandler}>关于</a>
+                	<a style={this.state.recommShow ? navStyle :  {} } href="#recommendation-page" name="recommShow" onClick={this.handleRecomm}>推荐</a>
+                	<a style={this.state.aboutShow ? navStyle :  {} } href="#about-page" name="aboutShow" onClick={this.handleAbout}>关于</a>
             	</div>
 
 				{/* 用户点击推荐页面 渲染推荐动画卡片 */}
